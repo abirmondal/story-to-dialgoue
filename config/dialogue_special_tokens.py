1f526fc00fb430a2ed5c72208bbe8860680bba5b
Abir Mondal <abir22197072@gmail.com>
2026-01-18 12:55:44 +0530

    Add update function for end-of-sequence token in dialogue special tokens

--- config/dialogue_special_tokens.py
+++ config/dialogue_special_tokens.py
@@ -1,5 +1,6 @@
 DIALOGUE_END_TOKEN = "<end_of_dialogue>" # Token indicating the end of a dialogue session
 DEFAULT_SEPARATOR_TOKEN = "</s>" # Default token used to separate different parts of the dialogue. This is the separator of BART and T5 models.
+DEFAULT_EOS_TOKEN = "</s>" # Default end-of-sequence token used in various models.
 
 def update_end_token(new_token: str) -> None:
     """
@@ -20,3 +21,13 @@ def update_sep_token(new_token: str) -> None:
     """
     global DEFAULT_SEPARATOR_TOKEN
     DEFAULT_SEPARATOR_TOKEN = new_token
+
+def update_eos_token(new_token: str) -> None:
+    """
+    Updates the default end-of-sequence token.
+
+    Args:
+        new_token (str): The new end-of-sequence token to be set.
+    """
+    global DEFAULT_EOS_TOKEN
+    DEFAULT_EOS_TOKEN = new_token
